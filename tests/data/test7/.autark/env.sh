2b36ddf21610b0c1a494a60ed8dffd9412b36dd4
Anton Adamansky <adamansky@gmail.com>
2025-06-03 23:57:27 +0700

    *

--- tests/data/test7/.autark/env.sh
+++ tests/data/test7/.autark/env.sh
@@ -3,10 +3,7 @@
 set -e
 
 which ar || { echo "'ar' not found in PATH"; exit 1; }
-which ld || { echo "'ld' not dound in PATH"; exit 1; }
 
 autark set AR=ar
-autark set LD=ld
 autark set BUILD_TYPE=Debug
-
 
