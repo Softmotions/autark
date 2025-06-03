067f707540d60469fe94e41ffa6d761698611176
Anton Adamansky <adamansky@gmail.com>
2025-06-03 23:03:36 +0700

    * test6

--- tests/test6.c
+++ tests/test6.c
@@ -50,6 +50,7 @@ int main(void) {
 
   //---
 
+  fprintf(stderr, "\n\n");
   chdir(cwd_prev);
   akassert(system("touch ./data/test6/hello.c") == 0);
   test_reinit(false);
@@ -72,6 +73,7 @@ int main(void) {
 
   //---
 
+  fprintf(stderr, "\n\n");
   chdir(cwd_prev);
   akassert(system("touch ./data/test6/hello.h") == 0);
   test_reinit(false);
@@ -95,8 +97,31 @@ int main(void) {
       xstr_ptr(xlog)) == 0
     );
 
-  // TODO: Check rebuild when context $ variable changed in flags
+  fprintf(stderr, "\n\n");
+  setenv("CFLAGS", "-O2", 1);
+  chdir(cwd_prev);
+  akassert(system("touch ./data/test6/hello.h") == 0);
+  test_reinit(false);
+  g_env.check.log = xstr_clear(xlog);
+  rc = script_open("./data/test6/Autark", &sctx);
+  akassert(rc == 0);
+  script_build(sctx);
+  script_close(&sctx);
 
+  akassert(
+    strcmp(
+      "Autark:36     cc: outdated \1-DBUILD_TYPE=Debug\1-DCURL_STATICLIB\1-DDEBUG=1\1-O0\1-g t=v f= \n"
+      "Autark:36     cc: outdated main.c t=a f=s\n"
+      "Autark:36     cc: outdated hello.c t=a f=s\n"
+      "Autark:36     cc: resolved outdated outdated=3\n"
+      "Autark:36     cc: build src=../main.c obj=main.o\n"
+      "Autark:36     cc: build src=../hello.c obj=hello.o\n"
+      "Autark:42    run: outdated main.o t=f f= \n"
+      "Autark:42    run: outdated hello.o t=f f= \n"
+      "Autark:42    run: resolved outdated outdated=2\n"
+      "Autark:42    run: cc\n",
+      xstr_ptr(xlog)) == 0
+    );
 
   xstr_destroy(g_env.check.log);
 }
