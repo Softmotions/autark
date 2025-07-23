65c1ed5958afc30b44158e2dd6b633f6e54d222f
Anton Adamansky <adamansky@gmail.com>
2025-07-23 22:51:19 +0700

    * minors

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(cd "$(dirname "$0")"; pwd -P)"
-pandoc --from gfm --standalone --highlight-style=monochrome --metadata title=Autark --include-in-header=./autark-css.html -o ./autark.html  ../README.md
+pandoc --from gfm --standalone --highlight-style=monochrome --metadata title=Autark --include-in-header=./autark-css.html -o ./index.html  ../README.md
