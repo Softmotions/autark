e8168dfe3c3e1665fbafd268999505fca1c67e2d
Anton Adamansky <adamansky@gmail.com>
2025-07-25 23:37:07 +0700

    * macos port

--- dist/build.sh
+++ dist/build.sh
@@ -6,7 +6,7 @@
 # https://github.com/Softmotions/autark
 
 META_VERSION=0.9.0
-META_REVISION=590d248
+META_REVISION=5d0174e
 cd "$(cd "$(dirname "$0")"; pwd -P)"
 
 prev_arg=""
@@ -62,7 +62,7 @@ cat <<'a292effa503b' > ${AUTARK_HOME}/autark.c
 #ifndef CONFIG_H
 #define CONFIG_H
 #define META_VERSION "0.9.0"
-#define META_REVISION "590d248"
+#define META_REVISION "5d0174e"
 #endif
 #define _AMALGAMATE_
 #define _XOPEN_SOURCE 700
